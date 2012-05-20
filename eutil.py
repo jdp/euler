ebf3720f745bf108bda65a82bbd3f96d2866f4ea
Justin Poliey <justin.d.poliey@gmail.com>
2012-05-20 04:32:12 -0400

    got u problem 27

--- eutil.py
+++ eutil.py
@@ -2,6 +2,36 @@ from math import sqrt, ceil
 from functools import partial
 
 
+class memoized(object):
+    """Decorator that caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned, and
+    not re-evaluated.
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+
+    def __call__(self, *args):
+        try:
+            return self.cache[args]
+        except KeyError:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+        except TypeError:
+            # uncachable -- for instance, passing a list as an argument.
+            # Better to not cache than to blow up entirely.
+            return self.func(*args)
+
+    def __repr__(self):
+        """Return the function's docstring."""
+        return self.func.__doc__
+
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        return partial(self.__call__, obj)
+
+
 def take(n, iterable):
     "Return first n items of the iterable as a list"
     from itertools import islice
@@ -54,54 +84,41 @@ def primes():
         q += 1
 
 
-def triangle_numbers(start=1):
+@memoized
+def is_prime(n):
+    for p in primes():
+        if p == n:
+            return True
+        if p > n:
+            return False
+
+
+def sequence(func, start=0):
     from itertools import count, imap
 
-    return imap(lambda n: n * (n + 1) / 2, count(start=start))
+    return imap(func, count(start=start))
+
+
+def triangle_numbers(start=1):
+    return sequence(lambda n: n * (n + 1) / 2, start=1)
 
 
 def rotations(lst):
     return [lst[i:] + lst[0:i] for i in range(len(lst))]
 
 
+@memoized
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
 
+@memoized
 def lcm(a, b):
     return (a * b) / gcd(a, b)
 
 
+@memoized
 def totient(n):
     return len([i for i in range(1, n) if gcd(i, n) == 1])
-
-class memoized(object):
-    """Decorator that caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned, and
-    not re-evaluated.
-    """
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        try:
-            return self.cache[args]
-        except KeyError:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-        except TypeError:
-            # uncachable -- for instance, passing a list as an argument.
-            # Better to not cache than to blow up entirely.
-            return self.func(*args)
-
-    def __repr__(self):
-        """Return the function's docstring."""
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        """Support instance methods."""
-        return partial(self.__call__, obj)
